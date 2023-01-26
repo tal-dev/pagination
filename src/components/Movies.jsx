118666d952fb4d83c22f33646b500127e3df54f6
tal-dev <talgat.abykeev@gmail.com>
2023-01-25 21:16:18 -0600

    mostly done

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -17,9 +17,6 @@ const Movies = () => {
         fetchdata(api)
     }, [])
 
-    // Example items, to simulate fetching from another resources.
-    // const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
     function Items({ currentItems }) {
         return (
             <div className="container">
@@ -56,25 +53,31 @@ const Movies = () => {
         return (
             <>
                 <ReactPaginate
-                    breakLabel="..."
                     nextLabel="next >"
                     onPageChange={handlePageClick}
-                    pageRangeDisplayed={5}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
                     pageCount={pageCount}
                     previousLabel="< previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
                     renderOnZeroPageCount={null}
                 />
                 <Items currentItems={currentItems} />
-
             </>
         );
     }
 
-    return (
-        <div>
-            <PaginatedItems itemsPerPage={4} />
-        </div>
-    )
+    return <PaginatedItems itemsPerPage={6} />
 
 }
 
