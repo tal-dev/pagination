ee260ef7fd2958c397bb3c73f1c04e189be5d846
tal-dev <talgat.abykeev@gmail.com>
2023-01-25 20:30:00 -0600

    wip

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const api = "https://api.sampleapis.com/movies/family"
 
 const Movies = () => {
     const [movies, setMovies] = useState([])
-    
+
+    const fetchdata = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+        setMovies(data)
+    }
+
+    useEffect(() => {
+        fetchdata(api)
+    }, [])
+
+    return movies.map(item => {
+        return <div className="movie-card">
+            <img src={item.posterURL} alt="" />
+            <div>{item.title}</div>
+        </div>
+    })
+
 }
 
 export default Movies
