7d2a5ed5b851c0dc50b2252fda7edfefd5578292
tal-dev <talgat.abykeev@gmail.com>
2023-01-25 20:19:45 -0600

    setup

---
+++ src/components/Movies.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+const api = "https://api.sampleapis.com/movies/family"
+
+const Movies = () => {
+    const [movies, setMovies] = useState([])
+    
+}
+
+export default Movies
